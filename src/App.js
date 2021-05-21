22201d0bff2a526128ed1458c3923d061a61a0f9
deepen77 <przemek.starostka@yahoo.com>
2021-05-21 18:36:18 +0100

    continue styling component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import Topbar from "./components/topbar/TopBar";
-import { BrowserRouter as Router,} from "react-router-dom";
-import Homepage from "./pages/home/Home";
-
+import TopBar from "./components/topbar/TopBar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    
     <Router>
-      <Topbar />
-      <Homepage/>
+      <TopBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">{user ? <Home /> : <Register />}</Route>
+        <Route path="/login">{user ? <Home /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">
+          {user ? <Settings /> : <Register />}
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
     </Router>
-  
   );
 }
 
