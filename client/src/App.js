5a67736d8045bc3ab28114d236dce6e585b2b4aa
deepen77 <przemek.starostka@yahoo.com>
2021-05-28 19:30:07 +0100

    complete project

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
+import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
-  const user = true;
+  const { user } = useContext(Context);
   return (
     <Router>
       <TopBar />
@@ -19,9 +21,7 @@ function App() {
         <Route path="/register">{user ? <Home /> : <Register />}</Route>
         <Route path="/login">{user ? <Home /> : <Login />}</Route>
         <Route path="/write">{user ? <Write /> : <Register />}</Route>
-        <Route path="/settings">
-          {user ? <Settings /> : <Register />}
-        </Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
         <Route path="/post/:postId">
           <Single />
         </Route>
